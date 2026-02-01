1821216f633b18b0a828539ee7b72804297ecc87
mischaOBMC <joson.mb@obmontessori.edu.ph>
2026-02-02 00:38:24 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,49 +1,28 @@
-from pyscript import document, display
+from pyscript import document
 
-subjects = ["Science", "Math", "English", "Fiipino", "ICT", "PE"]
-units = [5, 5, 5, 3, 1, 1]
+def Operation(event):
+    num1 = float(document.getElementById("num1").value)
+    num2 = float(document.getElementById("num2").value)
 
-def calculate_GWA(e):
+    result = (num1 + num2) / 2
 
-    Fname = document.getElementById("Fname").value
-    Lname = document.getElementById("Lname").value
+    document.getElementById("result").innerText = "Average: " + str(result)
 
-    Science = float(document.getElementById("Science").value or 0)
-    Math = float(document.getElementById("Math").value or 0)
-    English = float(document.getElementById("English").value or 0)
-    Filipino = float(document.getElementById("Filipino").value or 0)
-    ICT = float(document.getElementById("ICT").value or 0)
-    PE = float(document.getElementById("PE").value or 0)
+    result = round(result, 2)
 
-    totalUnits = units[0] + units[1] + units [2] + units[3] + units [4] + units [5]
-    totalWeight = (Science * units[0]) + (Math * units[1]) + (English * units[2]) + (Filipino * units[3]) + (ICT * units[4]) + (PE * units[5])
-    GWA = totalWeight / totalUnits
-
-    document.getElementById("name").innerHTML = (f"Name: {Fname} {Lname}")
-
-    document.getElementById("sci").innerHTML = (f"{subjects[0]}: {Science}")
-    document.getElementById("math").innerHTML = (f"{subjects[1]}: {Math}")
-    document.getElementById("english").innerHTML = (f"{subjects[2]}: {English}")
-    document.getElementById("fil").innerHTML = (f"{subjects[3]}: {Filipino}")
-    document.getElementById("ict").innerHTML = (f"{subjects[4]}: {ICT}")
-    document.getElementById("pe").innerHTML = (f"{subjects[5]}: {PE}")
-
-
-    document.getElementById("gwa").innerHTML = (f"Your GWA is {GWA:.2f}")
-
-    GWA = round(GWA, 2)
-
-    if 94 <= GWA <= 100:
-        level = "Bergamo I"
-    elif 87 <= GWA < 94:
-        level = "Bergamo II"
-    elif GWA < 87 and GWA >= 80:
+    if 94 <= result and result <= 100:
+        level = "is Bergamo I"
+    elif 87 <= result and result < 94:
+        level = "is Bergamo II"
+    elif result < 87 and result >= 80:
         level = "is Bergamo III"
-    elif GWA < 80 and GWA >= 75:
+    elif result < 80 and result >= 75:
         level = "is Perugia I"
-    elif GWA < 75 and GWA >= 65:
+    elif result < 75 and result >= 65:
         level = "is Perugia II"
     else:
         level = "cannot be classified"
 
-    document.getElementById("grade_class").innerHTML = f"Your level {level}"
+    document.getElementById("result2").innerHTML = f"Your level {level}"
+
+
